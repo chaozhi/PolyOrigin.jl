c850d4e0ce1cece266e8f791b72d16d24f1f4ead
Zheng, Chaozhi <chaozhi.zheng@wur.nl>
2021-04-27 17:35:00 +0200

    eg

---
+++ example/example.jl
@@ -0,0 +1,32 @@
+using PolyOrigin
+cd(@__DIR__)
+pwd()
+# run polyorigin
+genofile = "geno.csv"
+pedfile = "ped.csv"
+outstem = "example_output"
+@time polyancestry = polyOrigin(genofile, pedfile;
+    refinemap=true,
+    refineorder=true,
+    isplot=true,
+    outstem,
+)
+
+# plot relative frequencies of valent configurations
+polyancestry = readPolyAncestry(outstem*"_polyancestry.csv")
+valentfreq = calvalentfreq(polyancestry)
+plotvalentfreq(valentfreq)
+
+# calculate the accuracy of parental phasing and ancestral inference
+truefile = "true.csv"
+truegeno = readTruegeno!(truefile, polyancestry)
+acc = calAccuracy!(truegeno, polyancestry)
+println(acc)
+
+# plot conditional probabilities
+plotCondprob(polyancestry, truegeno = truegeno, offspring = 1)
+animCondprob(polyancestry;
+    truegeno = truegeno,
+    fps = 0.5,
+    outfile = string(outstem,"_condprob.gif"),
+)
